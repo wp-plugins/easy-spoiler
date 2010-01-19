1898ebe06ac24dba22e495064c913e8763deec3b
dyerware <dyerware@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-19 12:33:34 +0000

    first version
    git-svn-id: https://plugins.svn.wordpress.org/easy-spoiler/trunk@195344 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ easy-spoiler.php
@@ -0,0 +1,185 @@
+<?php
+/*
+Plugin Name: Easy Spoiler
+Version: 0.1
+Plugin URI: http://www.dyerware.com/main/products/easy-spoiler
+Description: Creates an attractive container to hide a spoiler within a post or page.
+Author: dyerware
+Author URI: http://www.dyerware.com
+*/
+/*  Copyright © 2009, 2010  dyerware
+    Support: support@dyerware.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+  
+class wpEasySpoiler
+{
+    private $spoilerNum = 0;
+    
+	public function __construct()
+    {         
+    }
+
+	function addCSS() 
+	{
+	   
+		echo '<link type="text/css" rel="stylesheet" href="' . plugins_url ( plugin_basename ( dirname ( __FILE__ ) ) ) .'/easy-spoiler.css" />';
+		
+	
+	}
+ 
+    private function translateNumerics(&$value, $key) {
+        
+        if ($value == 'false') {
+        	$value = false;
+        } elseif ($value == 'true') {
+            $value = true;
+        }
+        
+    }        
+            
+	public function process_shortcode($atts, $content=null, $code="") 
+	{  
+	   	$haveIssue = FALSE;
+	    $nearKey = "";
+	    $nearValue = "";
+	    
+	    if ($atts)
+	    {
+    	    foreach ($atts as $key => $att)
+    	    {
+    	       $keyval = (int)$key;
+    	       if ($keyval != 0 || strpos($key, "0") === 0)
+    	       {
+                    $haveIssue = TRUE;
+                    $nearKey = $keyval;
+                    $nearValue = $att;
+                    break;
+    	       }
+    	    }
+	    }
+
+	    if ($haveIssue === TRUE)
+	       return "<p><b>EASY SPOILER SHORTCODE ERROR</b><lu><li>Check for misspelled parameters (case matters)</li><li>Check for new lines (all must reside on one long line)</li><li>Error near [" . $key . "], [" . $att . "]</li></lu><br/>For assistance, please visit <a>http://www.dyerware.com/main/products/easy-spoiler</a></p>";
+	       
+        $spoilerConfig = shortcode_atts( array(
+                'intro' => 'Spoiler Inside',
+                'title' => '',
+                'tablecss' => 'easySpoilerWrapper')
+			    , $atts );
+
+
+	    // Translate strings to numerics
+	    array_walk($spoilerConfig, array($this, 'translateNumerics'));
+	         
+	       	       
+	    $this->spoilerNum++;
+		$rowDiv = 'spoilerDiv' . $this->spoilerNum;
+
+		$rowDiv2 = '"' . $rowDiv . '"';
+		$spoilerbutton = "'" . $rowDiv . '_action' . "'";
+		
+		if ($spoilerConfig['title'] === '')
+		{
+		  $spoilertitle = $spoilerConfig['intro'];
+		}
+		else
+		{
+		  $spoilertitle = $spoilerConfig['intro'] . ': ' . $spoilerConfig['title'];
+		}
+		  
+		$tableCSS = "'" . $spoilerConfig['tablecss'] . "'";			
+	
+	    if (is_null($content))
+	    {
+	       $scontent = "(no spoiler)";
+	    }
+        else
+        {
+           $scontent = do_shortcode($content);
+           
+           /*
+           	if (function_exists('json_encode')) {
+        	   $jscontent = json_encode($scontent);
+            } else {
+			   require_once('json_encode.php');
+		    }
+		    */
+        }
+
+        $button = "'wpSpoilerToggle(" . $rowDiv2 . ");'";
+	
+        return <<<ecbCode
+<div class={$tableCSS}>
+
+<table class='easySpoilerTable' border='0' style='text-align:center;' align='center' bgcolor='FFFFFF'>
+
+<head>
+<tr><th class='easySpoilerTitleA'  style='text-align:left;vertical-align:middle;font-size:120%'>{$spoilertitle}</th>
+<th class='easySpoilerTitleB'  style='text-align:right;vertical-align:middle;font-size:100%'><INPUT type='button' id={$spoilerbutton} class='easySpoilerButton' value='Show' onclick={$button} align='right'></th>
+</th></tr>
+</head>
+
+
+<tr><td class='easySpoilerRow' colspan='2'><div id={$rowDiv} class='easySpoilerSpoils' style='display:none; white-space:wrap; vertical-align:middle;'>
+{$scontent}
+</div></td></tr>
+</table>
+
+
+<div class='easySpoilerConclude'><table class='easySpoilerTable'  border='0' style='text-align:center;' frame='box' align='center' bgcolor='FFFFFF'>
+<tr><th class='easySpoilerEnd' style='width:100%'></th>
+<td class='easySpoilerEnd' style='white-space:nowrap;' colspan='2'></td></tr>
+                   
+<tr><td class='easySpoilerEnd' colspan='2'></td></tr>
+</table>
+</div>
+
+</div>
+<script type="text/javascript">
+//<![CDATA[
+function wpSpoilerToggle(id) {
+    var myName = id + '_action';
+    var me = document.getElementById(myName);
+    var e = document.getElementById(id);
+    
+    if(e.style.display == 'block')
+    {
+        e.style.display = 'none';
+        me.value='Show';
+    }
+    else
+    {        
+        e.style.display = 'block';
+        me.value='Hide';
+    }
+}
+//]]>
+</script>
+ecbCode;
+   }
+}  
+
+// Instantiate our class
+$wpEasySpoiler= new wpEasySpoiler();
+
+/**
+ * Add filters and actions
+ */
+
+add_action('wp_head', array($wpEasySpoiler, 'addCSS'));
+add_shortcode('spoiler',array($wpEasySpoiler, 'process_shortcode'));
+?>
---
+++ readme.txt
@@ -0,0 +1,64 @@
+=== Easy Spoiler ===
+
+Contributors: dyerware
+Donate link: http://www.dyerware.com/main/products/easy-spoiler/easy-spoiler-plugin-for-wordpress.html
+Tags: spoiler,hint,tip,answer,hidden,hide,mobile,shortcode,dyerware
+Requires at least: 2.8
+Tested up to: 2.9.1
+Stable tag: 0.1
+
+This plugin allows you to easily create a container for a spoiler within your blog by use of shortcodes.
+
+
+== Description ==
+
+This plugin allows you to easily insert spoilers into your blog, making use shortcodes. An attractive container containing a hint to its content is created with a show/hide button.  The container may contain other shortcakes within it.
+
+PHP5 Required.
+
+The shortcode format is:
+
+**[spoiler title="Secret location of the code"]It's in the dresser[/spoiler]**
+
+For a complete example and more detailed documentation, visit the plugin home page.  If you do not understand these instructions, please ask questions in the comments section on the plugin home page.  We will be more than happy to answer them.
+
+Go here for up-to-date usage and examples:
+[EasySpoiler Tutorial](http://www.dyerware.com/main/products/easy-spoiler/easy-spoiler-plugin-for-wordpress.html "EasySpoiler Tutorial")
+
+
+== Installation ==
+
+1. Verify that you have PHP5, which is required for this plugin.
+
+2. Upload the `easy-chart-builder` directory to the `/wp-content/plugins/` directory
+
+3. Activate the plugin through the 'Plugins' menu in WordPress
+
+Now you can insert the shortcodes within your posts and pages.
+
+
+== Frequently Asked Questions ==
+
+For an up-to-date FAQ, please visit:
+[EasySpoiler FAQ](http://www.dyerware.com/main/products/easy-spoiler/easy-spoiler-faq.html "EasySpoiler FAQ")
+
+== Screenshots ==
+
+1. A plain spoiler, opened
+
+2. A spoiler with title hint, opened
+
+3. A spoiler with custom intro, title hint, and complex contents (in this case, easy chart builder) 
+
+
+== Upgrade Notice ==
+
+= 0.1 =
+First version
+
+
+== Changelog ==
+
+= 0.1 =
+
+ * First public release
