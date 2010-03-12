71bcb6c66b92b6f1e5a0573e3b316d59ecfbbd5b
dyerware <dyerware@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-12 23:51:22 +0000

    taggin 0.5
    git-svn-id: https://plugins.svn.wordpress.org/easy-spoiler/tags/0.5@216795 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dyerware-adm.php
+++ dyerware-adm.php
@@ -2,7 +2,7 @@
 if ( !defined('ABSPATH') )
 	exit('Sorry, you are not allowed to access this page directly.');
 
-$infomercials = array(
+$helplinks = array(
 	array(
 		'text' => 'Tutorial',
 		'url' => "http://www.dyerware.com/main/products/easy-spoiler/easy-spoiler-plugin-for-wordpress.html",
@@ -21,6 +21,23 @@ $infomercials = array(
 		'icon' => "http://www.dyerware.com/images/favorites.png" ),
 );
 
+$infomercials = array(
+	array(
+		'text' => 'Gallery and Caption',
+		'desc' => 'Upgrade your wordpress galleries and captioned images with animations and effects',
+		'url' => "http://www.dyerware.com/main/products/gallery-and-caption/gallery-and-caption-plugin-for-wordpress.html", 
+		'icon' => "http://www.dyerware.com/images/checkmark.png" ),
+	array(
+		'text' => 'Easy Review Builder',
+		'desc' => 'Create styled star-based review summary boxes',
+		'url' => "http://www.dyerware.com/main/products/easy-review-builder/easy-review-builder.html",
+		'icon' => "http://www.dyerware.com/images/checkmark.png" ),
+	array(
+		'text' => 'Easy Chart Builder',
+		'desc' => 'Create a variety of colorful charts in posts and widgets',
+		'url' => "http://www.dyerware.com/main/products/easy-chart-builder/easy-chart-builder-plugin-for-wordpress.html",
+		'icon' => "http://www.dyerware.com/images/checkmark.png" ),		
+);
 ?>
 <style type="text/css">
 	.wp-admin form, .wp-admin div.updated {
@@ -49,16 +66,30 @@ $infomercials = array(
 </style>
 
 <div class="dyerware-adminfobar">
-	<center>plugin by <strong>dyerware</strong></center>
+	<center>Support</center>
 	<hr size="0" />
 	<ul>
-<?php foreach ($infomercials as $each) : unset($hr) ; extract($each) ?>
+<?php foreach ($helplinks as $each) : unset($hr) ; extract($each) ?>
 		<?php if ($hr) : ?><hr size="0" /><?php endif ?>
 		<li style="list-style-image:url(<?php echo $icon ?>)">
 		<a target="_blank" href="<?php echo $url ?>" ><?php echo $text ?></a>
 		</li>
 <?php endforeach ?>
 	</ul>
+
+    <div style="height:20px"></div>
+	<center>plugins by <strong>dyerware</strong></center>
+	<hr size="0" />
+	<ul>	
+<?php foreach ($infomercials as $each) : unset($hr) ; extract($each) ?>
+		<?php if ($hr) : ?><hr size="0" /><?php endif ?>
+		<li style="list-style-image:url(<?php echo $icon ?>)">
+		<a target="_blank" href="<?php echo $url ?>" ><?php echo $text ?></a>
+		</li><?php echo $desc ?>
+<?php endforeach ?>
+	</ul>
+	
+	<div style="height:20px"></div>
 	<hr size="0" />
 	<center><small>
 		<!-- License --><?php if (@!$license) $license = 'GPL'; ?>
